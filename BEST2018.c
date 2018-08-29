b7957289cce4118cd582ff266f4c673000736b9d
jkmosa01 <jordanmosakowski@gmail.com>
2018-08-29 12:03:36 -0600

    remove old code Fixes#1

--- BEST2018.c
+++ BEST2018.c
@@ -1,13 +1,10 @@
-//Right now this is the same as last year's code but will be edited later.
-
 //which port on the robot we            Name of the motor. 
 //plug it in. It is important           You can name this whatever 
 //to put the driving motors             you want and it is used 
 //on opposite sides to provide          later to control the motors
 //enough power          ↓                   ↓                                               
-#pragma config(Motor,  port2,           rightMotor,    tmotorServoContinuousRotation, openLoop, reversed)
-#pragma config(Motor,  port9,           leftMotor,     tmotorServoContinuousRotation, openLoop)
-#pragma config(Motor,  port6,           smallMotor,   tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port2,           moveMotor,    tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port9,           swivel,     tmotorServoContinuousRotation, openLoop)
 #pragma config(Motor,  port5,           servo,        tmotorServoStandard, openLoop)
 
 task main()
@@ -31,33 +28,14 @@ task main()
 
 
 		//Driving Motor Control - Port 2/9, basic driving
-		if (direction == 1){
-		motor[leftMotor] = vexRT[Ch3] /driveSpeed;
-		motor[rightMotor] = vexRT[Ch2] /driveSpeed;}
-		else if (direction == 0){
-		motor[leftMotor] = vexRT[Ch2] /driveSpeed*-1;
-		motor[rightMotor] = vexRT[Ch3] /driveSpeed*-1;}
+		/*if (direction == 1){
+		motor[leftMotor] = vexRT[Btn] /driveSpeed;
+        }*/
+        //put the new code to control robot on beam below
 
         //Driving Direction, lets us drive backwards
 		if (vexRT[Btn8U]){direction=1;}
-		if (vexRT[Btn8D]){direction=0;}
-
-		//Grabber - Port 5, opens and closees gripper
-		if (vexRT[Btn6U] == 1){
-			motor[servo] = -127;
-		}
-		else if (vexRT[Btn6D] == 1){
-			motor[servo] =50;
-		}
-
-		//Arm Movement - Port 6, moves gripper arm up and down.
-		if (vexRT[Btn5D] == 1){
-			motor[smallMotor] = -100;
-		}
-		else if (vexRT[Btn5U] == 1){
-			motor[smallMotor] = 100;
-		}
-		else {motor[smallMotor] = 0;}
+		if (vexRT[Btn8D]){direction=-1;}
 
 
 	}
