4328c84ac99846e71e853f783609366293901e5e
CorbinWEstes <42982952+CorbinWEstes@users.noreply.github.com>
2018-09-20 15:51:51 -0600

    Update BEST2018.c

--- BEST2018.c
+++ BEST2018.c
@@ -1,6 +1,10 @@
 #pragma config(Motor,  port2,           moveMotor,    tmotorServoContinuousRotation, openLoop)
 #pragma config(Motor,  port9,           swivel,     tmotorServoContinuousRotation, openLoop)
 #pragma config(Motor,  port5,           servo,        tmotorServoStandard, openLoop)
+#pragma config(Motor,  port6,		tilt1,     tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port7,		tilt2,     tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port4,           clawArm1,        tmotorServoStandard, openLoop)
+#pragma config(Motor,  port8,           clawArm2,        tmotorServoStandard, openLoop)
 
 task main()
 {
@@ -8,34 +12,50 @@ task main()
 	driveSpeed=3;
 	int direction;
 	direction=1;
+	int servoTilt = 0;
+	
 	while(true)
     //define all variables before the while loop, and put all things that can change at any time below (mostly button presses and motor control)
 	{
 		//Driving Speed, This is used for allowing us to change the speed of our robot.
 		if(vexRT[Btn7U] == 1)
-		{driveSpeed=1;}
+		{driveSpeed = 1;}
 		else if(vexRT[Btn7L] == 1)
-		{driveSpeed=2;}
+		{driveSpeed = 2;}
 		else if(vexRT[Btn7R] == 1)
-		{driveSpeed=3;}
+		{driveSpeed = 3;}
 		else if(vexRT[Btn7D] == 1)
-		{driveSpeed=4;}
+		{driveSpeed = 4;}
 
-
-		//Driving Motor Control - Port 2/9, basic driving
-		/*if (direction == 1){
-		motor[leftMotor] = vexRT[Btn] /driveSpeed;
-        }*/
-
+		//Arm Movement
+		motor[swivel] = vexRT[Ch4];
+		motor[tilt1] = vexRT[Ch3];		
+		motor[tilt2] = vexRT[Ch2];
+		
+		if(vexRT[Btn5D]){motor[servo] = servoTilt + driveSpeed;}
+		else if(vexRT[Btn6D]){motor[servo] = servoTilt - driveSpeed;}
+        	else{motor[servo] = servoTilt;}
+		
+		if(vexRT[Btn8L]){
+			motor[clawArm1] = 0;
+			motor[clawArm2] = 0
+		}
+		else if(vexRT[Btn8R]){
+			motor[clawArm1] = 63;
+			motor[clawArm2] = -63;
+		}
+        	else{motor[servo] = servoTilt;} 
+		
         //put the new code to control robot on beam below, try to make it so that you can swap which buttons moves the robot one way (with variable direction)
+	
+        	if(vexRT[Btn5U]){motor[moveMotor] = direction * 128 / driveSpeed;}
+        	else if(vexRT[Btn6U]){motor[moveMotor] = direction * -128 / driveSpeed;}
+        	else{motor[moveMotor]=0;}
 
-        if(vexRT[Btn5U]){motor[moveMotor] = direction * 128 / driveSpeed;}
-        else if(vexRT[Btn6U]){motor[moveMotor] = direction * -128 / driveSpeed;}
-        else{motor[moveMotor]=0;}
 
         //Driving Direction, lets us drive backwards
-		if (vexRT[Btn8U]){direction=1;}
-		if (vexRT[Btn8D]){direction=-1;}
+		if (vexRT[Btn8U]){direction = 1;}
+		if (vexRT[Btn8D]){direction = -1;}
 
 
 	}
